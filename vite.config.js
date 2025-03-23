47583c70d4b2d3471354bd84d732e4200bd0dead
Priyanshu <priyanshu@Priyanshus-MacBook-Air.local>
2025-03-23 15:42:51 +0530

    removing tailwind config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // This is important!
-  ],
-  theme: {
-    extend: {},
-  },
-  base: "/bytestore/",
-  plugins: [react(), tailwindcss()],
-})
+  base: "/bytestore/", // Ensure correct base path for GitHub Pages
+  plugins: [react()], // Tailwind doesn't need to be here
+});
